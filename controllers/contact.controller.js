e5631a2208b93c072d7698a586488e5068662d1d
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-05-12 15:08:24 +0530

    Mail Sending Process Completed

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,5 +1,6 @@
 const dotenv=require('dotenv');
 const Contact = require('../models/contact.model');
+const nodemailer = require('nodemailer')
 dotenv.config({path:"./.env"})  
 const contactController=async(req,res)=>{
     try {
@@ -7,13 +8,30 @@ const contactController=async(req,res)=>{
         if (!name || !email || !subject || !detail) {
           return res.status(402).json({message:"Please fill all the fields"});
         }
+        const transporter = nodemailer.createTransport({
+          host: process.env.TRANSPORTER_HOST,
+          port: process.env.TRANSPORTER_PORT,
+          auth: {
+            user: process.env.TRANSPORTER_USER,
+            pass: process.env.TRANSPORTER_PASS
+          }
+        });
+          const mailOptions={
+            from: "work.aryanpamwani@gmail.com", 
+            to: email, 
+            subject:"Thanks For contacting us.", 
+            html: "<b>We Will Reach Out You Sooner</b>", 
+          };
+          const emailResponse = await transporter.sendMail(mailOptions);
         const saveContact=await Contact.create({
             name,
             email,
             subject,
             detail
         });
-        return res.status(200).json({message:"Contact Saved Successfully ",data:saveContact});
+        
+          
+        return res.status(200).json({message:"Contact Saved Successfully ",mail:emailResponse,data:saveContact});
     } catch (error) {
         console.log(error);
     }
